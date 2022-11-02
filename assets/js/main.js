6a694604d93a893e920955ef9437aaf1c2e0cf7e
Salvatore-Gianfrancesco <salvatore.gianfrancesco@yahoo.it>
2022-11-02 13:06:26 +0100

    added chat list with v-for

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,7 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
+            latestMessage: "",
             contacts: [
                 {
                     name: 'Michele',
